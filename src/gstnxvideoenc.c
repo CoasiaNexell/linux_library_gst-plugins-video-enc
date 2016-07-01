8637ea56e3a65073f29093e66cde2621936c774f
Sungwon Jo <doriya@nexell.co.kr>
2016-07-01 19:56:46 +0900

    gst-plugins-video-enc: Enable meta information for buffer acceleration.
    This patch enable meta information for buffer acceleration
    and remove unnecessary code.
    
    Change-Id: I4cad6edd6298632a8107a433814f7481bd31ec67
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>

--- src/gstnxvideoenc.c
+++ src/gstnxvideoenc.c
@@ -58,8 +58,6 @@
 #include <gstmmvideobuffermeta.h>
 #include "gstnxvideoenc.h"
 
-#define ENABLE_MMVIDEOBUFFER_META	0
-
 GST_DEBUG_CATEGORY_STATIC (gst_nxvideoenc_debug_category);
 #define GST_CAT_DEFAULT gst_nxvideoenc_debug_category
 
@@ -1097,10 +1095,7 @@ gst_nxvideoenc_handle_frame( GstVideoEncoder *encoder, GstVideoCodecFrame *frame
 	GstNxvideoenc *nxvideoenc = GST_NXVIDEOENC (encoder);
 	GstVideoFrame inframe;
 
-#if ENABLE_MMVIDEOBUFFER_META
 	GstMMVideoBufferMeta *meta = NULL;
-#endif
-
 	GstMapInfo in_info;
 	GstMapInfo out_info;
 
@@ -1111,21 +1106,13 @@ gst_nxvideoenc_handle_frame( GstVideoEncoder *encoder, GstVideoCodecFrame *frame
 
 	gst_video_codec_frame_ref( frame );
 
-#if ENABLE_MMVIDEOBUFFER_META
 	meta = gst_buffer_get_mmvideobuffer_meta( frame->input_buffer );
 	if( NULL != meta && meta->memory_index >= 0 )
-#else
-	if( 2 <= gst_buffer_n_memory(frame->input_buffer) )
-#endif
 	{
 		GstMemory *meta_block = NULL;
 		MMVideoBuffer *mm_buf = NULL;
 
-#if ENABLE_MMVIDEOBUFFER_META
 		meta_block = gst_buffer_peek_memory( frame->input_buffer, meta->memory_index );
-#else
-		meta_block = gst_buffer_peek_memory( frame->input_buffer, 0 );
-#endif
 		if( !meta_block )
 		{
 			GST_ERROR("Fail, gst_buffer_peek_memory().\n");
